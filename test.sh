e834374176bd47178721e39fe95384787f573f21
bekozi <benkoziol@gmail.com>
2016-03-18 15:02:14 -0600

    CLN: Removed additional .idea files

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 echo '+++ Serial tests +++'
-py.test --f -q -m 'not mpi_only' src
+py.test -q -m 'not mpi_only' src
 echo ''
 
 echo '+++ MPI tests ++++++'
-mpirun -n 8 py.test --f -q -m 'mpi' src
+mpirun -n 8 py.test -q -m 'mpi' src
 
