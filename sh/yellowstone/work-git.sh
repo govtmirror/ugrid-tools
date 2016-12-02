2325659e0bad1e16c422cde9ee68a12a17469ebf
bekozi <benkoziol@gmail.com>
2016-12-02 12:31:13 -0700

    wip: run key = nhd-exact-250m-regular-128

--- sh/yellowstone/work-git.sh
+++ sh/yellowstone/work-git.sh
@@ -1,10 +1,2 @@
 #!/usr/bin/env bash
 
-BRANCH=conus-spherical-exact-data
-
-cd ~/src/ugrid-tools && \
- git fetch && \
- git checkout ${BRANCH} && \
- git pull && \
-
-bash /glade/u/home/benkoz/src/ugrid-tools/sh/yellowstone/work.sh
--- sh/yellowstone/work.sh
+++ sh/yellowstone/work.sh
@@ -1,27 +1,14 @@
 #!/usr/bin/env bash
 
-
-#WD=/glade/u/home/benkoz/logs/esmf
-
-#cd ~/logs/esmf && \
-# bsub < /glade/u/home/benkoz/src/utools/sh/yellowstone/run-vpu.bsub && \
-# watch -n 5 bjobs
-
-#cd ${WD} && \
-#export J=conus-spherical-exact-data-regular2
-export J=conus-spherical-regular-256
-echo "Job name is: ${J}"
-
+export J=nhd-exact-250m-regular-128
+export BRANCH=dev-250m
 export UTOOLS_SRC=/glade/u/home/benkoz/src/ugrid-tools
 UTOOLS_SH=${UTOOLS_SRC}/sh/yellowstone/jobs/run.sh
-#UTOOLS_SH=/glade/u/home/benkoz/src/utools/sh/yellowstone/run-vpu.bsub
-#UTOOLS_SH=/glade/u/home/benkoz/src/utools/sh/yellowstone/run-vpu-texas.bsub
 
+cd ~/src/ugrid-tools && \
+ git fetch && \
+ git checkout ${BRANCH} && \
+ git pull && \
+
+echo "Job name is: ${J}"
 bash ${UTOOLS_SH}
-
-#bsub < ${UTOOLS_SH}
-
-#bsub -n 40 -J "utools-2.0.1" < ${UTOOLS_SH}
-#bsub -n 80 -J "utools-2.5.2" < ${UTOOLS_SH}
-#bsub -n 120 -J "utools-2.2.1" < ${UTOOLS_SH}
-#bsub -n 160 -J "utools-2.7.0" < ${UTOOLS_SH}
