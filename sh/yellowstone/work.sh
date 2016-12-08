3aa036b7a3f683dd1c023c95e7ffdb24f5bcf45a
bekozi <benkoziol@gmail.com>
2016-12-08 07:43:40 -0700

    wip: nhd-exact-250m-regular-256-1-nc4

--- sh/yellowstone/work.sh
+++ sh/yellowstone/work.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export J=nhd-exact-250m-regular-512-nc4
+export J=nhd-exact-250m-regular-256-1-nc4
 export BRANCH=dev-250m
 export UTOOLS_SRC=/glade/u/home/benkoz/src/ugrid-tools
 UTOOLS_SH=${UTOOLS_SRC}/sh/yellowstone/jobs/run.sh
