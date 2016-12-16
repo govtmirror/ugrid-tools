b695b3644437d1267ba13505b1b3e8a599ddb1a7
bekozi <benkoziol@gmail.com>
2016-12-16 11:39:22 -0700

    wip: nhd-250m-512-debug-gnu

--- sh/yellowstone/work.sh
+++ sh/yellowstone/work.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export J=nhd-250m-512-gnu
+export J=nhd-250m-512-debug-gnu
 export BRANCH=dev-250m
 export UTOOLS_SRC=/glade/u/home/benkoz/src/ugrid-tools
 UTOOLS_SH=${UTOOLS_SRC}/sh/yellowstone/jobs/run.sh
